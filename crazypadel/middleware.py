78e93b436929060c0e9e02500c2f261c151817d2
dariovenz <dariovenanzi.dev@gmail.com>
2025-02-21 11:45:34 +0100

    ok

---
+++ crazypadel/middleware.py
@@ -0,0 +1,14 @@
+# Crea un nuovo file: crazypadel/middleware.py
+class NoCacheBrowserBackMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        
+        # Aggiungi header per prevenire la cache del browser
+        response['Cache-Control'] = 'no-cache, no-store, must-revalidate, max-age=0'
+        response['Pragma'] = 'no-cache'
+        response['Expires'] = '0'
+        
+        return response
--- crazypadel/settings.py
+++ crazypadel/settings.py
@@ -33,6 +33,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'crazypadel.middleware.NoCacheBrowserBackMiddleware',  # Aggiungi questa riga
+
 ]
 
 ROOT_URLCONF = 'crazypadel.urls'
